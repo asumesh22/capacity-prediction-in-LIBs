00626326e304e5790f5d95380f7610b6bc4b8100
Aaryan Sumesh <asumesh22@gmail.com>
2024-10-09 11:20:49 -0400

    removed oxford data

--- csv_nasa.py
+++ csv_nasa.py
@@ -1,7 +1,7 @@
 from scipy.io import loadmat
 import pandas as pd
 
-data = loadmat(r"original_data\Nasa\1. BatteryAgingARC-FY08Q4\B0005.mat")
+data = loadmat(r".\Nasa\1. BatteryAgingARC-FY08Q4\B0005.mat")
 data = {k: v for k, v in data.items() if not k.startswith('_')}
 myData = {k:{'t':[], 'v':[], 'a':[], 'T':[]} for k in data}
 
